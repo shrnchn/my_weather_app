8c3a84b886505fb7f5df9921cdfd437476202ca9
schan21 <schan21@live.com>
2014-03-31 17:42:38 -0400

    initial commit

--- weather_script.js
+++ weather_script.js
@@ -18,13 +18,6 @@ $(function() {
 				$('.weather').remove();
 			}
 
-			if (!!data.current_observation) {
-				$('.holder').append('°C');
-				console.log("it works");
-				$('.imageholder').prepend('<img alt="weather icon" class="icon">');
-				console.log("it works!!!");
-			}
-
 			var w = data.current_observation;
 			var forecast = w.weather;
 			var location = w.display_location.full;
@@ -40,20 +33,96 @@ $(function() {
 			var weathericon = w.icon_url;
 			var icon = w.icon;
 
-			console.log(icon);
-			if (temp >= 20) {
-				$('.activities').text("It's a hot day out, refresh yourself with an ice tea!");
-			}
-			else if (temp > 5) {
-				$('.activities').text("It's kinda chilly, try a bubbletea!");
-			}
-			else if (temp < 0) {
-				$('.activities').text("Brrr it's cold, try making a hot chocolate to warm up!");
+			if (!!data.current_observation) {
+				$('.holder').text('°C');
+				console.log("it works");
+
+
+					if (forecast === "Chance of Flurries") {
+						$('.imageholder').html('<img src="icons/chanceofflurries.svg">');
+					}
+					else if (forecast === "Chance of Rain") {
+						$('.imageholder').html('<img src="icons/chanceofrain.svg">');
+					}
+					else if (forecast === "Chance Rain") {
+						$('.imageholder').html('<img src="icons/chanceofrain.svg">');
+					}
+					else if (forecast === "Chance of Freezing Rain") {
+						$('.imageholder').html('<img src="icons/freezingrain.svg">');
+					}
+					else if (forecast === "Chance of Sleet") {
+						$('.imageholder').html('<img src="icons/freezingrain.svg">');
+					}
+					else if (forecast === "Chance of Snow") {
+						$('.imageholder').html('<img src="icons/chanceoffluries.svg">');
+					}
+					else if (forecast === "Chance of Thunderstorms") {
+						$('.imageholder').html('<img src="icons/chanceofthunderstorms.svg">');
+					}
+					else if (forecast === "Chance of a Thunderstorm") {
+						$('.imageholder').html('<img src="icons/chanceofthunderstorms.svg">');
+					}
+					else if (forecast === "Clear") {
+						$('.imageholder').html('<img src="icons/sunny.svg">');
+					}
+					else if (forecast === "Cloudy") {
+						$('.imageholder').html('<img src="icons/cloudy.svg">');
+					}
+					else if (forecast === "Flurries") {
+						$('.imageholder').html('<img src="icons/flurries.svg">');
+					}
+					else if (forecast === "Fog") {
+						$('.imageholder').html('<img src="icons/fog.svg">');
+					}
+					else if (forecast === "Haze") {
+						$('.imageholder').html('<img src="icons/fog.svg">');
+					}
+					else if (forecast === "Mostly Cloudy") {
+						$('.imageholder').html('<img src="icons/cloudy.svg">');
+					}
+					else if (forecast === "Mostly Sunny") {
+						$('.imageholder').html('<img src="icons/clear.svg">');
+					}
+					else if (forecast === "Partly Cloudy") {
+						$('.imageholder').html('<img src="icons/partlycloudy.svg">');
+					}
+					else if (forecast === "Partly Sunny") {
+						$('.imageholder').html('<img src="icons/partlycloudy.svg">');
+					}
+					else if (forecast === "Freezing Rain") {
+						$('.imageholder').html('<img src="freezingrain.svg">');
+					}
+					else if (forecast === "Rain") {
+						$('.imageholder').html('<img src="rain.svg">');
+					}
+					else if (forecast === "Sleet") {
+						$('.imageholder').html('<img src="icons/freezingrain.svg">');
+					}
+					else if (forecast === "Snow") {
+						$('.imageholder').html('<img src="icons/flurries.svg">');
+					}
+					else if (forecast === "Sunny") {
+						$('.imageholder').html('<img src="icons/sunny.svg">');
+					}
+					else if (forecast === "Thunderstorms") {
+						$('.imageholder').html('<img src="icons/thunderstorm.svg">');
+					}
+					else if (forecast === "Thunderstorm") {
+						$('.imageholder').html('<img src="icons/thunderstorm.svg">');
+					}
+					else if (forecast === "Unknown") {
+						$('.imageholder').html('<img src="icons/unknown.svg">');
+					}
+					else if (forecast === "Overcast") {
+						$('.imageholder').html('<img src="icons/overcast.svg">');
+					}
+					else if (forecast === "Scattered Clouds") {
+						$('.imageholder').html('<img src="icons/cloudy.svg">');
+					}
 			}
 
 			// dump string into the divs
 			$('.forecastLocation').text(forecast + ' in ' + location);
-			$('.icon').attr('src', weathericon);
 			$('.temp').text(tempRounded);
 			$('.feelsLike').text('Feels like ' + feelsRounded +'°C');
 			$('.wds').text('Wind: '+ windDirection + ' ' + windSpeed + ' km/h');
@@ -61,8 +130,6 @@ $(function() {
 			$('.h').text('Humidity: ' + humidity);
 		}
 	}); // end ajax
-
-
 }; // end getTheWeather
 
 	// run the function in the form
@@ -75,5 +142,6 @@ $(function() {
 		$('.weather').addClass('show');
 
 	}); // end form
+
 
 });
